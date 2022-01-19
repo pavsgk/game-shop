cc525c5001bf73434b14cd82e5a7d38fccf6a262
Alex Martyan <alexanderthemartian@gmail.com>
2022-01-19 17:58:47 +0200

    ops: addition mongoURL

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'Сюда копируем URI для доступа в БД, например: mongodb+srv://username:password@..........................',
+    'mongodb+srv://DanItProjectUserName:qwerty2022@cluster0.pgj5l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
