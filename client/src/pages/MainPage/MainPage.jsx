5e5b98faf981184e28d3a5da3c56b373bbbb0293
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2022-01-25 22:32:17 +0200

    add payment and delivery components

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,16 +1,9 @@
-import OrderingComponent from '../../components/OrderingComponent/OrderingComponent';
 import React from 'react';
 
 function MainPage() {
   return (
     <div>
       <h1>Main Page</h1>
-
-      <OrderingComponent
-        text={'Standart'}
-        details={'normally 4-5 business days, unless otherwise noted'}
-        price={'$7.95'}
-      />
     </div>
   );
 }
