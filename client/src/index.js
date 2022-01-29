a4df29ce89313bfa68f5377bfa0d253f30d86386
Alex Martyan <alexanderthemartian@gmail.com>
2022-01-29 12:48:26 +0200

    feat: add redux store and logic for cart

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {Provider} from 'react-redux';
+import store from '../src/store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
