2e0fee2ec7e6f2274e4cb71f79e3de8560f66fbc
ASkrynnik <88550983+ASkrynnik@users.noreply.github.com>
2022-01-30 11:27:00 +0200

    feat: linked products page to backend, create placeholder for products page (#13)

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,16 @@
-import styles from "./ProductCard.module.scss"
+import styles from './ProductCard.module.scss';
 
-function ProductCard() {
+function ProductCard(props) {
+  const {item} = props;
+  const {title, imageUrls, currentPrice} = item;
+
   return (
     <div className={styles.cardContainer}>
-        <div className={styles.iconWrapper}>
-          <img src="https://content1.rozetka.com.ua/goods/images/big_tile/221644260.jpg" alt="Fifa 22 PC" />
-        </div>
-        <h3 className={styles.productDecription}>Игра FIFA 22 для ПК</h3>
-        <span className={styles.price}>&#8372; 1999</span>
+      <div className={styles.iconWrapper}>
+        <img src={imageUrls[0]} alt={title} height="250" width="220" />
+      </div>
+      <h3 className={styles.productDecription}>{title}</h3>
+      <span className={styles.price}>&#8372; {currentPrice}</span>
     </div>
   );
 }
