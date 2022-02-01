7736c8ef3dae26e6154f15fd3e155d141fca6e4e
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2022-02-01 22:33:04 +0200

    tabs styles

--- client/src/components/Delivery/Delivery.jsx
+++ client/src/components/Delivery/Delivery.jsx
@@ -1,17 +1,30 @@
 import styles from './Delivery.module.scss';
 import OrderingComponent from '../OrderingComponent/OrderingComponent';
+import Button from '../Button/Button';
 import React from 'react';
 
 function Delivery() {
   return (
     <div className={styles.delivery}>
-      <OrderingComponent
-        text={'Standart'}
-        details={'normally 4-5 business days, unless otherwise noted'}
-        price={'$7.95'}
-      />
-      <OrderingComponent text={'Express'} details={'in Kyiv only'} price={'$10.95'} />
-      <OrderingComponent text={'Courier'} details={'in Kyiv only'} price={'$16.95'} />
+      <input type="radio" name="delivery" id="standart" value="delivery" />
+      <label htmlFor="standart">
+        <OrderingComponent
+          text={'Standart'}
+          details={'normally 4-5 business days, unless otherwise noted'}
+          price={'$7.95'}
+        />
+      </label>
+      <input type="radio" name="delivery" id="express" value="delivery" />
+      <label htmlFor="express">
+        <OrderingComponent text={'Express'} details={'in Kyiv only'} price={'$10.95'} />
+      </label>
+      <input type="radio" name="delivery" id="courier" value="delivery" />
+      <label htmlFor="courier">
+        <OrderingComponent text={'Courier'} details={'in Kyiv only'} price={'$16.95'} />
+      </label>
+      <div className={styles.btnNext}>
+        <Button type="submit">Next</Button>
+      </div>
     </div>
   );
 }
--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CheckoutPage from '../CheckoutPage/CheckoutPage';
 
 function MainPage() {
   return (
