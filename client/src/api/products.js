eb24d299986a7b0d962cfff7841c888f5ef44dbc
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2022-02-02 00:26:09 +0200

    feat: add getFilteredProducts function

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -13,3 +13,24 @@ export async function getProduct(itemNo) {
   const {data} = await instance.get(`products/${itemNo}`);
   return data;
 }
+
+export async function getFilteredProducts(querystring) {
+  let query = '';
+  if (typeof querystring === 'object') {
+    for (const [key, value] of Object.entries(querystring)) {
+      if (Array.isArray(value)) {
+        if (!value.length) continue;
+        query += `${key}=${value.join(',')}&`;
+      } else {
+        if (value) query += `${key}=${value}&`;
+      }
+    }
+  } else {
+    query = querystring;
+  }
+
+  if (query[query.length - 1] === '&') query = query.slice(0, -1);
+
+  const {data} = await instance.get(`products/filter?${query}`);
+  return data;
+}
