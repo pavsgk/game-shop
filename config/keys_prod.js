451dfc2a0bb613c411c70ec0963f8876a41eff96
Pavlo Sapatsynskyi <45953133+pavsgk@users.noreply.github.com>
2022-02-05 02:07:58 +0200

    Heroku setup (#21)
    * Payment and delivery components (#6)
    
    * add variables (#2)
    
    feat: add variables
    
    * feat: create Product page with styles for mobile res, add background to main container, add route /catalog (#3)
    
    * Header (#4)
    
    * add variables
    
    * add header mobile version
    
    * add header tablet v
    
    * fix header styles
    
    * fix breakpoints
    
    Co-authored-by: Pavlo Sapatsynskyi <45953133+pavsgk@users.noreply.github.com>
    
    * add ordering component
    
    * add payment and delivery components
    
    Co-authored-by: Pavlo Sapatsynskyi <45953133+pavsgk@users.noreply.github.com>
    Co-authored-by: ASkrynnik <88550983+ASkrynnik@users.noreply.github.com>
    
    * feat: add Heroku support
    
    * feat: Heroku test branch
    
    * fix: server
    
    Co-authored-by: KaterynaRom <97855689+KaterynaRom@users.noreply.github.com>
    Co-authored-by: ASkrynnik <88550983+ASkrynnik@users.noreply.github.com>

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,9 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
+  // mongoURI:
+  // 'mongodb+srv://DanItProjectUserName:qwerty2022@cluster0.pgj5l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPassword: process.env.NODEMAILER_PASSWORD,
   nodemailerService: process.env.NODEMAILER_SERVICE
