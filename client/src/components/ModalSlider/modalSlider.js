c2276861b2360c79ab804ad7ccf7562d2506dd0c
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2022-02-17 01:12:49 +0200

    genres

--- client/src/components/ModalSlider/modalSlider.js
+++ client/src/components/ModalSlider/modalSlider.js
@@ -1,18 +1,19 @@
-import SlideShow from 'react-image-show-fork-new';
+// import SlideShow from 'react-image-show-fork-new';
 
 const ModalSlider = ({modalContent}) => {
-  return (
-    <SlideShow
-      images={modalContent}
-      width="920px"
-      imagesWidth="100%"
-      imagesHeight="450px"
-      imagesHeightMobile="65vw"
-      indicators
-      fixedImagesHeight
-      infinite={true}
-    />
-  );
+  return null;
+  // return (
+  //   <SlideShow
+  //     images={modalContent}
+  //     width="920px"
+  //     imagesWidth="100%"
+  //     imagesHeight="450px"
+  //     imagesHeightMobile="65vw"
+  //     indicators
+  //     fixedImagesHeight
+  //     infinite={true}
+  //   />
+  // );
 };
 
 export default ModalSlider;
