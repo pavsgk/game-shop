5d8ab74dd697b9c8a0c09e0e1f960dc1711c80fa
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2022-02-17 12:32:34 +0200

    genres - svg

--- client/src/components/Genres/Genres.jsx
+++ client/src/components/Genres/Genres.jsx
@@ -1,24 +1,13 @@
 import styles from './Genres.module.scss';
 import React, {useEffect, useState} from 'react';
 import {getAllFilters} from '../../api/filters';
-import {ReactComponent as Action} from './img/Action.svg';
-// import {ReactComponent as Arcade} from './img/Arcade.svg';
-// import {ReactComponent as Indie} from './img/Indie.svg';
-// import {ReactComponent as MMO} from './img/MMO.svg';
-// import {ReactComponent as RPG} from './img/RPG.svg';
-// import {ReactComponent as Shooters} from './img/Shooters.svg';
-// import {ReactComponent as Simulator} from './img/Simulator.svg';
-// import {ReactComponent as Sport} from './img/Sport.svg';
-// import {ReactComponent as Strategy} from './img/Strategy.svg';
-// import {ReactComponent as For_two} from './img/two1.svg';
 
 function GenresComponent(props) {
   const {genre} = props;
-  const genreImg = './img/' + genre + '.svg';
+  const genreImg = './genres_img/' + genre + '.svg';
   return (
     <>
       <div className={styles.genre}>
-        {/*<Action className={styles.img}/>*/}
         <img className={styles.img} src={genreImg} alt={genre} />
         <p>{genre}</p>
       </div>
@@ -47,50 +36,6 @@ function Genres() {
             })
           : null}
       </div>
-
-      {/*<p className={styles.title}>genres</p>*/}
-      {/*<div className={styles.wrapper}>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Action className={styles.img} />*/}
-      {/*    <p>action</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Arcade className={styles.img} />*/}
-      {/*    <p>arcade</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Indie className={styles.img} />*/}
-      {/*    <p>indie</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <MMO className={styles.img} />*/}
-      {/*    <p>MMO</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <RPG className={styles.img} />*/}
-      {/*    <p>RPG</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Shooters className={styles.img} />*/}
-      {/*    <p>shooters</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Simulator className={styles.img} />*/}
-      {/*    <p>simulator</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Sport className={styles.img} />*/}
-      {/*    <p>sport</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Strategy className={styles.img} />*/}
-      {/*    <p>strategy</p>*/}
-      {/*  </div>*/}
-      {/*  <div className={styles.genre}>*/}
-      {/*    <Two className={styles.img} />*/}
-      {/*    <p>for two</p>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </>
   );
 }
