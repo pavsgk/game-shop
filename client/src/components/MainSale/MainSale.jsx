f088189b17113bdd214609134480a3b938c8ca21
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2022-04-12 23:17:32 +0300

    fix salePage, mainPage, mainSlider

--- client/src/components/MainSale/MainSale.jsx
+++ client/src/components/MainSale/MainSale.jsx
@@ -2,13 +2,14 @@ import styles from './MainSale.module.scss';
 import MainComponent from '../MainComponent/MainComponent';
 import React, {useEffect, useState} from 'react';
 import instance from '../../api/instance';
+import {Link} from 'react-router-dom';
 import {ReactComponent as Sale} from './img/sale.svg';
 
 function MainSale(props) {
   const {isMain = true} = props;
   const [saleProd, setSaleProd] = useState([]);
   const sale = [];
-  let x = null;
+  let saleMain = null;
 
   useEffect(() => {
     (async () => {
@@ -26,20 +27,20 @@ function MainSale(props) {
       });
     }
     if (isMain) {
-      x = sale.slice(0, 12);
+      saleMain = sale.slice(0, 12);
     } else {
-      x = sale;
+      saleMain = sale;
     }
   })();
 
   return (
     <>
-      <div className={styles.title}>
+      <Link exact="true" to="sale" className={styles.title}>
         sale
         <Sale className={styles.titleSvg} />
-      </div>
+      </Link>
       <div className={styles.wrapper}>
-        {x.map((el) => {
+        {saleMain.map((el) => {
           return (
             <React.Fragment key={el.itemNo}>
               <div className={styles.componentWrapper}>
