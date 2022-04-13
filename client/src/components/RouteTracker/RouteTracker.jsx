17cba1639e887a3009d94f19f20f21b109889d6e
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2022-04-14 00:23:18 +0300

    feat: prev next button

--- client/src/components/RouteTracker/RouteTracker.jsx
+++ client/src/components/RouteTracker/RouteTracker.jsx
@@ -1,6 +1,7 @@
 import {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import {useLocation} from 'react-router-dom';
+import {pushHistory} from '../../store/reducers/routerReducer';
 import {setResultsVisibility} from '../../store/reducers/searchReducer';
 
 function RouteTracker({children}) {
@@ -8,6 +9,7 @@ function RouteTracker({children}) {
   const location = useLocation();
 
   useEffect(() => {
+    dispatch(pushHistory(location));
     dispatch(setResultsVisibility(false));
   }, [location, dispatch]);
 
